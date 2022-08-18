21a5a5b0993f71f005c0a870af7bf84ff202c1e3
Wassim Mansouri <wassim.mansouri@gmail.com>
2022-08-18 22:33:15 +0200

    Print errors to the standard error output.

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|error| {
-        println!("Problem parsing arguments: {}", error);
+        eprintln!("Problem parsing arguments: {}", error);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
